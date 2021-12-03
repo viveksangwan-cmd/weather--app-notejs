296f368b3cce0678d1118c37fb2f1914d32970b8
vivek sangwan <viveksangwan006@gmail.com>
2021-12-03 14:23:31 +0530

    code splited in files in util and done with destructuring

--- app.js
+++ app.js
@@ -1,51 +1,20 @@
-import request from "postman-request";
+import { geocode } from "./utils/geocode.js";
+import { forecast } from "./utils/forecast.js";
 
-const weatherUrl =
-  "http://api.weatherstack.com/current?access_key=a0d8537925a43dfd05640e2ba451dfcc&query=37.8267,-122.4233";
-
-request({ url: weatherUrl, json: true }, (err, response) => {
-  if (err) {
-    console.log("Unable to connect with the weather service.");
-  } else if (response.body.error) {
-    console.log("Unable to find the location.");
-  } else {
-    const temp = response.body.current.temperature;
-    const precip = response.body.current.precip;
-    const str = `It is currently ${temp} degree out. There is a ${precip}% chance of rain.`;
-    console.log(str);
-  }
-});
-
-const geocode = (address, callback) => {
-  const coordinateUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1Ijoidml2ZWtzYW5nd2FuIiwiYSI6ImNrd3B5azNxczBob3Uyd3FvOWRxdHV6ZmMifQ.TR6d2AdJuV4m8MnuwLrqaA&limit=1`;
-  request({ url: coordinateUrl, json: true }, (err, response) => {
+const location = process.argv[2];
+if (!location) {
+  console.log("Please provide a location.");
+} else {
+  geocode(location, (err, data) => {
     if (err) {
-      callback("Unable to connect to location services.", undefined);
-    } else if (response.body.features.length === 0) {
-      callback(
-        "Unable to find the coordinates for location. Try another search.",
-        undefined
-      );
-    } else {
-      const latitude = response.body.features[0].geometry.coordinates[0];
-      const longitude = response.body.features[0].geometry.coordinates[1];
-      const location = response.body.features[0].place_name;
-      callback(undefined, {
-        latitude: latitude,
-        longitude: longitude,
-        location: location,
-      });
+      return console.log(err);
     }
+    forecast(data.latitude, data.longitude, (err, forecastData) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    });
   });
-};
-
-geocode("London", (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
-
-console.log("Staring");
-console.log("Ending");
+}
